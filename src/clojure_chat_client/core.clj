0f1eca8807033848af809b4bc8a0052a3e6e706a
Marius Butuc <marius.butuc@gmail.com>
2015-03-21 15:23:16 -0400

    Connect to the server
      * using `clj-sockets`

--- src/clojure_chat_client/core.clj
+++ src/clojure_chat_client/core.clj
@@ -1,7 +1,14 @@
 (ns clojure-chat-client.core
+  (:require [clj-sockets.core])
   (:gen-class))
 
 (defn -main
-  "I don't do a whole lot ... yet."
+  "The hello world of chat clients"
   [& args]
-  (println "Hello, World!"))
+
+  (def socket (clj-sockets.core/create-socket "localhost" 1234))
+
+  (clj-sockets.core/write-line socket "USER Marius")
+  (clj-sockets.core/write-line socket "MSG Hello world!")
+
+  (clj-sockets.core/close-socket socket))
